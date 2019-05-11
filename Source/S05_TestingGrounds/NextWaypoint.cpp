b690d535447fc830a12dadaa8593c6e2b89c9a85
konstantin_maiuk <maiuk.kostiantyn@gmail.com>
2019-05-11 02:23:58 +0200

    New component for Route Points. Now logic is fully works in C++

--- Source/S05_TestingGrounds/NextWaypoint.cpp
+++ Source/S05_TestingGrounds/NextWaypoint.cpp
@@ -2,20 +2,27 @@
 
 #include "NextWaypoint.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
+#include "TP_ThirdPerson/TP_ThirdPersonCharacter.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 
 EBTNodeResult::Type UNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	// TODO protect against empty patrol roots
+	// Get Patrol route
+	APawn* ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	UPatrolRoute* PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
 
-	// Get Patrol points
-	AAIController* AIController = OwnerComp.GetAIOwner();
-	APawn* ControlledPawn = AIController->GetPawn();
-	APatrollingGuard* PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	TArray<AActor*> PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
 
+	// TODO protect against empty patrol routes
+	TArray<AActor*> PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points"));
+		return EBTNodeResult::Failed;
+	}
+	
 	// Set Next Waypoint
 	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
 	int32 Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
@@ -23,10 +30,9 @@ EBTNodeResult::Type UNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp
 
 	// Cycle Index
 	int NextIndex = (Index + 1) % PatrolPoints.Num();
-	BlackboardComp->SetValueAsInt(WaypointKey.SelectedKeyName, NextIndex);
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
 
-
-	UE_LOG(LogTemp, Warning, TEXT("waypoint index %i"), Index);
+	//UE_LOG(LogTemp, Warning, TEXT("waypoint index %i"), Index);
 	
 	return EBTNodeResult::Succeeded;
 }
